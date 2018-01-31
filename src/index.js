99e0157dc57cba2dde4b6e50b2d440acb1705a7e
Kaiichiro Ota <kigh.ota@gmail.com>
2018-01-31 11:33:35 +0900

    データをコマンドラインから指定する

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 const CsvLoader = require('./CsvLoader');
 const PdfGenerator = require('./PdfGenerator/PdfGenerator');
 const NENGA_POSTCARD_LAYOUT = require('./PdfGenerator/Layout').NENGA_POSTCARD;
-const testdata = require('./testdata');
+const program = require('commander');
+const fs = require('fs');
 
-const entries = new CsvLoader().loadCsv('./test.csv');
+program
+  .option('--to <toCsvPath>', '宛先データ(CSV) [必須]')
+  .option('--from <fromJsonPath>', '差出人データ(JSON) [必須]')
+  .parse(process.argv);
+
+if (!program.to || !program.from) {
+  program.outputHelp();
+  process.exit(1);
+}
+
+const fromJson = fs.readFileSync(program.from, {encoding: 'utf-8'});
+const from = JSON.parse(fromJson);
+
+const entries = new CsvLoader().loadCsv(program.to);
+
 PdfGenerator.generate(entries.map(entry => {
-  return {to: entry, from: testdata.json.from};
+  return {to: entry, from};
 }), NENGA_POSTCARD_LAYOUT, 'fonts/ipaexm.ttf');
